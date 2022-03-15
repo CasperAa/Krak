8febfe63d22a200715e27bbae96f0d8186e17d50
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-03-15 01:25:52 +0100

    Midlertidigt testdata lavet

--- src/main/java/backend/configuration/MakeTestData.java
+++ src/main/java/backend/configuration/MakeTestData.java
@@ -1,4 +1,42 @@
 package backend.configuration;
 
-public class MakeTestData {
+import backend.entity.Member;
+import backend.repository.MemberRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Profile("!test")
+public class MakeTestData implements ApplicationRunner {
+
+    MemberRepository memberRepository;
+
+    public MakeTestData(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void makePMembers(){
+        Member m1 = memberRepository.save(new Member("amanda@amanda.dk", "Amanda", "Amandasen", "70121416"));
+        Member m2 = memberRepository.save(new Member("casper@casper.co.uk", "Casper", "Caspersen", "88888888"));
+
+        System.out.println("########################################################################################");
+        System.out.println("########################################################################################");
+        System.out.println("#################################### WARNING ! #########################################");
+        System.out.println("## This part breaks a fundamental security rule -> NEVER ship code with default users ##");
+        System.out.println("########################################################################################");
+        System.out.println("########################  REMOVE BEFORE DEPLOYMENT  ####################################");
+        System.out.println("########################################################################################");
+        System.out.println("########################################################################################");
+        System.out.println("Created TEST Users");
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        makePMembers();
+
+    }
 }
+
