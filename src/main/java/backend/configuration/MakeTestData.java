b57a6483dd51cbeb60414d72a8595acc57e42e75
amandatolstruplaursen <amanda.t.laursen@hotmail.com>
2022-03-15 01:30:09 +0100

    Mindre rettelser

--- src/main/java/backend/configuration/MakeTestData.java
+++ src/main/java/backend/configuration/MakeTestData.java
@@ -17,25 +17,15 @@ public class MakeTestData implements ApplicationRunner {
         this.memberRepository = memberRepository;
     }
 
-    public void makePMembers(){
+    public void makeMembers(){
         Member m1 = memberRepository.save(new Member("amanda@amanda.dk", "Amanda", "Amandasen", "70121416"));
         Member m2 = memberRepository.save(new Member("casper@casper.co.uk", "Casper", "Caspersen", "88888888"));
-
-        System.out.println("########################################################################################");
-        System.out.println("########################################################################################");
-        System.out.println("#################################### WARNING ! #########################################");
-        System.out.println("## This part breaks a fundamental security rule -> NEVER ship code with default users ##");
-        System.out.println("########################################################################################");
-        System.out.println("########################  REMOVE BEFORE DEPLOYMENT  ####################################");
-        System.out.println("########################################################################################");
-        System.out.println("########################################################################################");
-        System.out.println("Created TEST Users");
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        makePMembers();
+        makeMembers();
 
     }
 }
