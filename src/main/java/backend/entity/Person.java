faa8670b49fdedc0bf943257d27ea9248d6b7971
isabelPatulski <isab985d@stud.kea.dk>
2022-03-17 09:36:40 +0100

    Fjernet kommentar i Person om contructor

--- src/main/java/backend/entity/Person.java
+++ src/main/java/backend/entity/Person.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @AllArgsConstructor
-//@NoArgsConstructor - intelli siger den allerede er defineret????
+@NoArgsConstructor
 public class Person {
     @Id
     private String email;
