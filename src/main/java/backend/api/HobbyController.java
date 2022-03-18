72d0beb939038d3bab7c44166954a601ef20822a
andreajiao <andreajiao@gmail.com>
2022-03-18 10:04:07 +0100

    prøver at få styr på hobbycontroller

--- src/main/java/backend/api/HobbyController.java
+++ src/main/java/backend/api/HobbyController.java
@@ -5,15 +5,26 @@ import backend.repository.HobbyRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 
+@RequestMapping("api/hobbies")
 public class HobbyController {
 
-    public static void main(String[] args) {
-        HobbyRepo h = new HobbyRepo();
-        System.out.println(h.getAllHobbies().toString());
+    HobbyRepo hobbyRepo= new HobbyRepo();
+
+    @GetMapping("/hobbylist")
+    public ArrayList<Hobby> allHobbies(){
+       return hobbyRepo.getAllHobbies();
     }
+
+
+
+    /*public static void main(String[] args) {
+        HobbyRepo h = new HobbyRepo();
+        System.out.println(h.getAllHobbies());
+    }*/
 
 
 }
