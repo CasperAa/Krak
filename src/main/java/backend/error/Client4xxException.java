06ec189f8204a701cdb0271a46229e9c8c684578
andreajiao <andreajiao@gmail.com>
2022-03-18 10:32:37 +0100

    login funktion fjernet

--- src/main/java/backend/error/Client4xxException.java
+++ src/main/java/backend/error/Client4xxException.java
@@ -1,8 +1,9 @@
 package backend.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-//@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class Client4xxException extends RuntimeException{
     HttpStatus status;
     public Client4xxException(String message) {
